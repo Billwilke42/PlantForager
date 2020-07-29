8c902fe8c29ba1f16efd5c5d9f91bbf426b96250
Bill Wilke <billwilke2@gmail.com>
2020-07-29 13:21:53 -0600

    Makes basic sections and styling for Nav

---
+++ src/CardContainer/CardContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './CardContainer.css'
+
+const CardContainer = () => {
+  return (
+    <section className='card-container'>
+      
+    </section>
+  )
+}
+
+export default CardContainer
--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,2 +1,12 @@
 import React from 'react';
-import 'Nav.css'
+import './Nav.css'
+
+const Nav = () => {
+  return (
+    <header className='nav-container'>
+
+    </header>
+  )
+}
+
+export default Nav
