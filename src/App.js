fa7c09b0e6fb295944c87b587ffe83debd9e5ac5
Bill Wilke <billwilke2@gmail.com>
2020-07-29 14:17:24 -0600

    Sets up route path

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom'
 import Nav from './Nav/Nav'
 import CardContainer from './CardContainer/CardContainer'
+import Search from './Search/Search'
 
 function App() {
   return (
@@ -11,7 +12,15 @@ function App() {
       <Switch>
         <Route exact path='/'>
           <Nav />
-            <CardContainer />
+          <CardContainer />
+        </Route>
+        <Route path='/search'>
+          <Search />
+          <CardContainer />
+        </Route>
+        <Route path='/favorites'>
+          <Nav />
+          <CardContainer />
         </Route>
       </Switch>
     </div>
