e619faabd53747bea3b87723ac095048bf622fce
Bill Wilke <billwilke2@gmail.com>
2020-07-31 16:49:07 -0600

    Finishes render for nav component for showing favorites and returning home

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -5,6 +5,21 @@ import { connect } from 'react-redux'
 import { setFavoritesPage } from '../actions'
 
 const Nav = (props) => {
+  if(props.favoritesPage) {
+    return (
+      <header className='nav-container'>
+      <h1>Plant Forager</h1>
+      <div className='button-section'>
+        {/* <Link to='/search'>
+          <button type='submit'>Search</button>
+        </Link> */}
+        <Link to='/'>
+          <button type='submit' onClick={() => props.returnHome()}>Home</button>
+        </Link>
+      </div>
+    </header>
+    )
+  }
   return (
     <header className='nav-container'>
       <h1>Plant Forager</h1>
