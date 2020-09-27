238f7aa33267fa3936d4280a01f373934f73948e
Bill Wilke <billwilke2@gmail.com>
2020-09-27 12:55:11 -0600

    updates url

--- src/thunks/getPlants.js
+++ src/thunks/getPlants.js
@@ -2,12 +2,12 @@ import { isLoading, hasErrored, setPlants } from '../actions'
 // const fetch = require('node-fetch')
 
 export const getPlants = (pageNum) => {
-  const url = `https://cors-anywhere.herokuapp.com/https://trefle.io/api/v1/species?token=pMAzkzZTgwt3C_hk6kdpRu6zVLfcZzqZpwD9w98Ppb8&page${pageNum}`
+  const url = `https://cors-anywhere.herokuapp.com/https://trefle.io/api/v1/plants?filter_not%5Bedible_part%5D=null&page=${pageNum}&token=pMAzkzZTgwt3C_hk6kdpRu6zVLfcZzqZpwD9w98Ppb8`
   
   return async (dispatch) =>  {
     try {
       dispatch(isLoading(true))
-      const response = await fetch(url, { header: 'Access-Control-Allow-Origin' })
+      const response = await fetch(url)
       if (!response.ok) {
         throw Error(response.statusText)
       }
